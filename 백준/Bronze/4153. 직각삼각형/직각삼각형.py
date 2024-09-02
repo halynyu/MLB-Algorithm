2a866b7063f3557ce33f4e6d1a73865b31f08b3f
하린 <120549760+halynyu@users.noreply.github.com>
2024-09-02 15:17:22 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,11 @@
+while True:
+    nums = list(map(int, input().split()))
+    if sum(nums) == 0:
+        break
+    else:
+        max_num = max(nums)
+        nums.remove(max_num)
+        if nums[0]**2 + nums[1]**2 == max_num**2:
+            print('right')
+        else:
+            print('wrong')
